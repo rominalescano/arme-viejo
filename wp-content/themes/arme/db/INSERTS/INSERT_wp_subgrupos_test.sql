d525508da2f29c2c2b0809fa59ec6effb6340d7e
rominalescano <romina.mariel.lescano@gmail.com>
2020-09-26 13:01:13 -0300

    inserts a la base de dato

---
+++ wp-content/themes/arme/db/INSERTS/INSERT_wp_subgrupos_test.sql
@@ -0,0 +1,65 @@
+-- inserts de la tabla  wp_subgrupos_test 
+
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 1,“Test de Inteligencia I.L.V.”,1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 2,“Test de Inteligencia I.L.V.”,1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 3,“Test de Inteligencia  I.N.”,1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 4,“Test de Inteligencia  I.V.E”,1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 5,“Test de Inteligencia  I.M.”,1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 6,“Test de Inteligencia  I.M.”,1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 7,“Test de Inteligencia  I.M.”,1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (1, 8,“Test de Inteligencia I.Inter.P”,1);
+
+
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 1,'Para un adulto es absolutamente necesario tener el cariño y la aprobación de sus semejantes, familia, amigos.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 2,'Debe ser indefectiblemente competente y casi perfecto en todo lo que emprende.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 3,'Ciertas personas son malas, viles y perversas y deberían ser castigadas.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 4,'Es horrible cuando las cosas no van como a uno le gustaría que fueran.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 5,'Los acontecimientos externos son la causa de la mayoría de las desgracias de la humanidad.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 6,'Se debe sentir miedo o ansiedad ante cualquier cosa desconocida, incierta o potencialmente peligrosa.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 7,'Es más fácil evitar los problemas y responsabilidades de la vida que hacerles frente.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 8,'Se necesita contar con algo más grande y más fuerte que uno mismo.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 9,'El pasado tiene gran influencia en la determinación del presente.',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (2, 10,'La felicidad aumenta con la inactividad, la pasividad y el ocio indefinido.',1);
+
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 1,'Artístico Plástica',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 2,'Cálculo',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 3,'Cientifica',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 4,'Ejecutivo Persuasiva',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 5,'Mecánico Constructiva',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 6,'Musical',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 7,'Oranización',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 8,'Servicio Social',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 9,'Destreza Manual',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (3, 10,'Verbal',1);
+
+
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 1,'Artístico Plástico',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 2,'Cálculo ',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 3,'Cientifico',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 4,'Ejecutivo Persuasiva',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 5,'Mecánico Constructiva',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 6,'Musical',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 7,'Organización',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 8,'Servicio Social',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 9,'Aire Libre',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (4, 10,'Verbal',1);
+
+
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (5, 1,'Activo',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (5, 2,'Teórico',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (5, 3,'Pragmatico',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (5, 4,'Reflexivo',1);
+
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (6, 1,'SAGUINEO',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (6, 2,'COLÉRICO',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (6, 3,'FLEMÁTICO ',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (6, 4,'MELANCÓLICO',1);
+
+
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (7, 1,'SAGUINEO',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (7, 2,'COLÉRICO',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (7, 3,'FLEMÁTICO ',1);
+insert into wp_subgrupos_test (tipo_test_id, subgrupo_test_id, nombre, estado) values (7, 4,'MELANCÓLICO',1);
+
+
+
---
+++ wp-content/themes/arme/db/INSERTS/INSERT_wp_tipo_test.sql
@@ -0,0 +1,11 @@
+-- inserts de wp_tipo_test
+
+INSERT INTO `wp_tipo_test` (`id`, `nombre`, `numero_test`, `texto`, `tipo_preguntas`, `descripcion_tipo_preguntas`, `ip`, `created_at`, `estado`) VALUES
+(1,	'Test de Inteligencias multiples',	'1',	'¡Aquí deberás tildar aquellas frases que te representen, tarta de ser sincero y ponerte en sintonía con tu personalidad e interior! ¡Vamos!',	'1',	'si/no',	NULL,	NULL,	1),
+(2,	'Creencias Limitantes - Test de Ellis',	'2',	'Este test debes completarlo sin analizar mucho cada respuesta, solo tildar aquella que te representa al momento de leerla.  ¡Recordar NO HAY RESPUESTAS CORRECTAS E INCORRECTAS- NI RESPUESTAS BUENAS O MALAS!',	'1',	'si/no',	NULL,	NULL,	1),
+(3,	'Aptitudes',	'3',	'¡En este test deberás tildar aquellas cosas que te gustan hacer!',	'2',	'del 0 al 4',	NULL,	NULL,	1),
+(4,	'Intereses',	'4',	'¡Este test también servirá para seleccionar aquellas cosas que te interesan y te apasionan!',	'2',	'del 0 al 4',	NULL,	NULL,	1),
+(5,	'Test Estilos de Aprendizaje',	'5',	'¡En este test necesito que pienses como actúas generalmente! NO TE OLVIDES QUE … NO HAY RESPUESTAS MALAS O EQUIVOCADAS',	'1',	'si/no',	NULL,	NULL,	1),
+(6,	'Test de Temperamento - Fortalezas',	'6',	'Aquí necesito pienses aquellos adjetivos que te definen',	'3',	'chequear todas las correctas',	NULL,	NULL,	1),
+(7,	'Test de Temperamento - Debilidades',	'7',	'Aquí necesito tildes los defectos o actitudes que te caracterizan ',	'3',	'chequear todas las correctas',	NULL,	NULL,	1),
+(8,	'Eneagrama',	'8',	'En este test corto deberás escoger 1 opción de las 3 que aparecen, si no te identificas 100% con ninguna tilda aquella que te represente en un 80%.  CON ESTE ULTIMO TEST ¡TERMINAMOS EL PROCESO!',	'1',	'si/no',	NULL,	NULL,	1);
